45f81b3be2ef8699267c7a363e9f33aea32d2b5c
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-11-20 16:13:05 +0100

    add js functions

---
+++ src/js/registModal.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+export const backdrop = document.querySelector('.regist-backdrop');
+export const modal = document.querySelector('.regist-form-container');
+
+const closeBtn = document.querySelector('[regist-close-btn]');
