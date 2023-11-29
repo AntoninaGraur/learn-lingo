486c81c1c91cc61546ffb71ee78983dd9fe11474
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-11-29 13:13:25 +0100

    try one more time

--- src/js/registModal.js
+++ src/js/registModal.js
@@ -1,13 +1,11 @@
 
 
+export const backdrop = document.querySelector('.regist-backdrop');
+export const modal = document.querySelector('.regist-form-container');
+export const openModal = document.querySelector('.registration-btn');
+export const closeBtn = document.querySelector('.regist-close-btn');
 
 
-
-
-
-
-
- const backdrop = document.querySelector('.regist-backdrop');
- const modal = document.querySelector('.regist-form-container');
-const openModal = document.querySelector('.registration-btn');
-const closeBtn = document.querySelector('regist-close-btn');
+openModal.addEventListener("click", () => {
+    backdrop.showModal()
+});
