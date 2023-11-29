d06c20f096e8b8b0400b0dc676eb09968e773471
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-11-29 17:00:45 +0100

    add modal window

--- src/js/registModal.js
+++ src/js/registModal.js
@@ -5,5 +5,3 @@ export const modal = document.querySelector('.regist-form-container');
 export const openModal = document.querySelector('.registration-btn');
 export const closeBtn = document.querySelector('.regist-close-btn');
 
-
-openModal.addEventListener("click", () => backdrop.classList.add("active"));
