b16baa2ad4c1e3cae1e9e4a95136b264b200f061
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-11-29 12:29:14 +0100

    start js for registration

--- src/js/registModal.js
+++ src/js/registModal.js
@@ -7,7 +7,7 @@
 
 
 
-export const backdrop = document.querySelector('.regist-backdrop');
-export const modal = document.querySelector('.regist-form-container');
-
-const closeBtn = document.querySelector('[regist-close-btn]');
+ const backdrop = document.querySelector('.regist-backdrop');
+ const modal = document.querySelector('.regist-form-container');
+const openModal = document.querySelector('.registration-btn');
+const closeBtn = document.querySelector('regist-close-btn');
