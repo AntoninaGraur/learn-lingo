02eee66569f6c738977ef1137deaf7aaa91096a0
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2024-01-03 14:51:34 +0100

    config firebase

--- src/js/authfirebase.js
+++ src/js/authfirebase.js
@@ -1,24 +1,22 @@
 
+  import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+  import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+  } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 
-import { getDatabase, set, ref } from 'firebase/database';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/app';
+  const firebaseConfig = {
+    apiKey: 'AIzaSyC4UcnUoTTN2_H0jlFuihTdTRN09u_I5Hg',
+    authDomain: 'vanillajavascript-8b86d.firebaseapp.com',
+    projectId: 'vanillajavascript-8b86d',
+    storageBucket: 'vanillajavascript-8b86d.appspot.com',
+    messagingSenderId: '722728904571',
+    appId: '1:722728904571:web:365b8fa812b48c9855ffde',
+  };
 
-
-import { initializeApp } from "firebase/app";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC4UcnUoTTN2_H0jlFuihTdTRN09u_I5Hg",
-  authDomain: "vanillajavascript-8b86d.firebaseapp.com",
-  projectId: "vanillajavascript-8b86d",
-  storageBucket: "vanillajavascript-8b86d.appspot.com",
-  messagingSenderId: "722728904571",
-  appId: "1:722728904571:web:365b8fa812b48c9855ffde"
-};
-
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase();
+  const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
 
